b7573bd1b09e51fec59696f5f4080c3bbe5f55c3
lesahley <lesashley56@gmail.com>
2017-05-23 16:57:08 -0500

    Agregando estilos

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,27 +2,6 @@ $(document).foundation()
 $('.sim-thumb').on('mouseover', function() {
   $('#main-product-image').attr('src', $(this).data('image'));
 })
-/*
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:true
-        },
-        600:{
-            items:3,
-            nav:false
-        },
-        1000:{
-            items:5,
-            nav:true,
-            loop:false
-        }
-    }
-})*/
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
@@ -35,20 +14,12 @@ $('.owl-carousel').owlCarousel({
     responsive:{
         0:{
             items:2
-
-          /*  mouseDrag : false,*/
-
-
         },
         600:{
             items:3
-          /*  mouseDrag :true,
-            touchDrag :true*/
         },
         1100:{
             items:5
-          /*  mouseDrag :true,
-            touchDrag :true*/
         }
     }
 })
